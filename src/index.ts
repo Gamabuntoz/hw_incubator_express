bb5b92e305d1fb63a322e088522c8ff26b6f07aa
Tsimofei <zhalianhouski@gmail.com>
2023-01-30 14:05:13 +0300

    Init commit 5hw

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {testingRouter} from "./routes/testing-router";
 import {blogsQueryRouter} from "./routes/blogs-routes/blogs-query-router";
 import {postsQueryRouter} from "./routes/posts-routes/posts-query-router";
 import {usersRouter} from "./routes/users-router/users-router";
+import {authRouter} from "./routes/auth-router";
 
 export const app = express()
 const port = process.env.PORT || 5000
@@ -19,6 +20,7 @@ app.use('/posts', postsCommandRouter)
 app.use('/posts', postsQueryRouter)
 app.use('/testing', testingRouter)
 app.use('/users', usersRouter)
+app.use('/auth', authRouter)
 
 const startApp = async () => {
     await runDb()
