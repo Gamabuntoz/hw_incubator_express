fb0e794ad4b5a8647ba2dce9c926c9e70e8af789
Tsimofei <zhalianhouski@gmail.com>
2023-01-30 13:56:18 +0300

    Init commit 5hw

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -1,11 +1,13 @@
 import {Request, Response, Router} from "express";
 import {postsService} from "../domain/posts-service"
 import {blogsService} from "../domain/blogs-service";
+import {usersService} from "../domain/users-service";
 
 export const testingRouter = Router()
 
 testingRouter.delete('/all-data', async (req: Request, res: Response) => {
     await postsService.deleteAllPosts()
     await blogsService.deleteAllBlogs()
+    await usersService.deleteAllUsers()
     res.sendStatus(204)
 })
