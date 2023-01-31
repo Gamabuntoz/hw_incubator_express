77ba796ac16273db760ac30e20c78af4ecdd75b7
Tsimofei <zhalianhouski@gmail.com>
2023-01-31 19:40:36 +0300

    Init commit 6hw

--- src/domain/comments-service.ts
+++ src/domain/comments-service.ts
@@ -1,10 +1,9 @@
-import {usersRepository} from "../repositories/users-repository";
 import {ObjectId} from "mongodb";
-import {commentsType, usersType} from "../repositories/types/types";
+import {commentsType} from "../repositories/types/types";
 import {commentsCollection} from "../repositories/db";
 import {commentsRepository} from "../repositories/comments/comments-repository";
 
-export const commentsService ={
+export const commentsService = {
     async findCommentById(commentId: ObjectId): Promise<commentsType | boolean | null> {
         const result = await commentsCollection.findOne({_id: commentId})
         if (!result) {
