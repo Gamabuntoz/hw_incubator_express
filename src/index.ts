71421530b6339aeea5794f4f557b41fe3477d239
Tsimofei <zhalianhouski@gmail.com>
2023-01-31 18:38:51 +0300

    Init commit 6hw

--- src/index.ts
+++ src/index.ts
@@ -9,19 +9,21 @@ import {postsQueryRouter} from "./routes/posts/posts-query-router";
 import {usersRouter} from "./routes/users/users-router";
 import {authRouter} from "./routes/auth/auth-router";
 import {commentsRouter} from "./routes/comments/comments-router";
+import cors from "cors"
 
 export const app = express()
 const port = process.env.PORT || 5000
 const parserMiddleware = bodyParser({})
-app.use(parserMiddleware)
 
-app.use('/blogs', blogsCommandsRouter)
-app.use('/blogs', blogsQueryRouter)
-app.use('/posts', postsCommandRouter)
-app.use('/posts', postsQueryRouter)
-app.use('/testing', testingRouter)
-app.use('/users', usersRouter)
-app.use('/auth', authRouter)
+app.use(cors({}))
+app.use(parserMiddleware)
+app.use("/blogs", blogsCommandsRouter)
+app.use("/blogs", blogsQueryRouter)
+app.use("/posts", postsCommandRouter)
+app.use("/posts", postsQueryRouter)
+app.use("/testing", testingRouter)
+app.use("/users", usersRouter)
+app.use("/auth", authRouter)
 app.use("/comments", commentsRouter)
 
 const startApp = async () => {
